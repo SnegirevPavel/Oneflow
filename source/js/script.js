33f021e3e8312061d5edc7573526acc8adc0e802
Pavel Snegirev <93034353+SnegirevPavel@users.noreply.github.com>
2022-01-18 16:06:35 +0700

    Обновил
    Закрепил шапку
    Отредактировал отступы для мобайл версии

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,9 @@ $('.mobile div').on('click', () => {
     $('.mobile nav').toggleClass('open');
     $('.mobile nav ul').toggleClass('show');
 });
-for (let a = 1; a <= $(".mobile ul li").length; a++){
-    $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
-}
 
 $(document).ready(function(){
-    $('.block__title').click(function(event){
+    $('.hide__title').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
     });
 });
