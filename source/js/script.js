dddec65552b73a0ba5fc833e49b8a40cd2c764bc
Pavel Snegirev <93034353+SnegirevPavel@users.noreply.github.com>
2022-01-20 02:09:14 +0700

    Add files via upload

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,27 @@ $(document).ready(function(){
         $(this).toggleClass('active').next().slideToggle(300);
     });
 });
+
+
+// Кнопка наверх----------
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let toTopBtn = document.querySelector('.to-up');
+
+    window.onscroll = function () {
+        if (window.pageYOffset > 780) {
+            toTopBtn.style.display = 'block'
+        } else {
+            toTopBtn.style.display = 'none'
+        }
+    }
+
+    // плавный скролл наверх 
+    toTopBtn.addEventListener('click', function () {
+        window.scrollBy({
+            top: -document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+});
